d5274c10be334a4bc85d86937773b0ee23dce638
Jimmy Leta <jimmy.leta@gmail.com>
2023-06-21 22:02:39 -0400

    Renamed test case

--- test/approval_tests.cpp
+++ test/approval_tests.cpp
@@ -3,7 +3,7 @@
 
 #include "panel/panel.h"
 
-TEST(panel_tests, INPR1) {
+TEST(PanelTests, FletcherEx1) {
   auto input = R"({"num_panels": 20, "mach_number": 0.0, "ellipse_ratio": 0.5})";
   const auto r = panel::Panel::run(input);
   ApprovalTests::Approvals::verify(r);
